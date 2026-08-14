8b05be504bb11c9f2f4e9ddeee74229ddce82330
Samuel Rodriguez <grd.rodrigez13@gmail.com>
2026-08-13 21:07:16 -0600

    Resizing Mac Screen
    Resizing mac screen

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,9 +4,16 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
+    var windowFrame = self.frame
+    
+    // Set a larger initial size (e.g. 800x600)
+    windowFrame.size = NSSize(width: 850, height: 650)
+    
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+    
+    // Set a minimum size to prevent the window from being resized too small
+    self.minSize = NSSize(width: 600, height: 500)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
